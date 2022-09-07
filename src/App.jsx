a8f07ab323506b238fd7497275bd4340ed15f9f6
grassfinn <theharper@live.com>
2022-09-07 00:51:39 -0400

    Name Changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import React from 'react'
 import './App.css'
-import Header from './components/Header'
 import Story from './components/Story'
 import storyOutline from './modules/storyData'
 import StoryData from './modules/story'
@@ -11,7 +10,7 @@ const storyPlot = new StoryData(storyOutline)
 function App() {
   return (
     <div className='app'>
-      <Header />
+      <Story />
       {/* <Story /> */}
     </div>
   )
