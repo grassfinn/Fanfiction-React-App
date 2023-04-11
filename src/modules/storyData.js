5de4cd52c4db47d770fa7d889ccc10b3f745b3f3
grassfinn <theharper@live.com>
2023-04-10 22:06:49 -0400

    Generate and store stories when snippets

--- src/modules/storyData.js
+++ src/modules/storyData.js
@@ -10,7 +10,8 @@ const storyOutline = {
     'Knuckles',
     'Tails',
   ],
-  plot: ['Day Off', 'At the Beach', 'At the Fair', 'Date', 'in Prison']
+  plot: ['Day Off', 'At the Beach', 'At the Fair', 'Date', 'in Prison'],
+  
 
 };
 
